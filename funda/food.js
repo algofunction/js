45af0af6cdc8c0f77b09ec4ca7a9148f8292fa30
Function <42390852+algofunction@users.noreply.github.com>
2025-07-23 23:59:29 +0200

    Update food.js

--- funda/food.js
+++ funda/food.js
@@ -1,3 +1,19 @@
+/*
+You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return an object which includes the count of food options selected by the developers on the meetup sign-up form..
+
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C', 
+    meal: 'vegetarian' },
+  { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript', 
+    meal: 'standard' },
+*/
+
+
+
 function orderFood(list) {
   var orders = {};
   list.forEach(d=>{
